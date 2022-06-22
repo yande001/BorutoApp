e53f7195914081ff1be90b350e450460fd563a58
Darren <sengerby300@gmail.com>
2022-06-22 23:37:07 +0800

    Setup Room and hilt

--- app/src/main/java/com/example/darren/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/darren/borutoapp/MainActivity.kt
@@ -15,7 +15,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.darren.borutoapp.navigation.SetupNavGraph
 import com.example.darren.borutoapp.ui.theme.BorutoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
