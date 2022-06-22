5fb646166e0cf4e646c22bd014ffad485f5262de
Darren <sengerby300@gmail.com>
2022-06-22 23:28:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/darren/borutoapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.darren.borutoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.darren.borutoapp.navigation.SetupNavGraph
+import com.example.darren.borutoapp.ui.theme.BorutoAppTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BorutoAppTheme {
+                // A surface container using the 'background' color from the theme
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/darren/borutoapp/domain/model/HeroRemoteKey.kt
@@ -0,0 +1,14 @@
+package com.example.darren.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.darren.borutoapp.util.Constants.HERO_REMOTE_KEY_DATABASE_TABLE
+
+
+@Entity(tableName = HERO_REMOTE_KEY_DATABASE_TABLE)
+data class HeroRemoteKey(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val prevPage: Int?,
+    val nextPage: Int?
+)
